3bb73ba8c62dc305802c027a641afd7137b41d3e
Kluyvert Ananias <kluyvertananias@hotmail.com>
2019-03-26 09:58:56 -0300

    comentários das alterações

--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -13,5 +13,65 @@ namespace App2
 		{
 			InitializeComponent();
 		}
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            //desabilitar mensagens
+            Label1.IsVisible = false;
+            Label2.IsVisible = false;
+            // verificar se o período e semestre foram selecionados
+            if(Entry2.Text != null &&
+               Entry2.Text.Legth > 0 &&
+               Picke.SelectedIndex >= 0)
+            {
+
+                // criar objeto aluno
+
+                Aluno aluno = new Aluno();
+
+                // modificar o período do aluno de acordo com o texto
+
+                aluno.periodo = int.Parse(Entry2.Text);
+
+                // verificar se os valores são iguais
+
+                if (aluno.periodo == disciplinas[Picker.SelectedIndex].semestre)
+
+                {
+
+                    // habilitar mensagem de erro
+
+                    Label1.IsVisible = true;
+
+                }
+                else
+
+                {
+
+                    // habilitar mensagem de erro
+
+                    Label2.IsVisible = true;
+
+                }
+
+            }
+            else
+
+            {
+
+                // habilitar mensagem de erro
+
+                Label2.IsVisible = true;
+
+            }
+
+
+
+        }
+
+
+    }
+
+
 	}
 }
